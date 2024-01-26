4769491ae50d809000c83cbd51572c6030051b06
mkmah <mah.manojk@gmail.com>
2024-01-26 16:46:48 +0530

    feat: add zellij

--- config/nvim/install.fish
+++ config/nvim/install.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 if not command -qs nvim
     exit
-endg
+end
 
 alias --save e='nvim'
 alias --save v='nvim'
---
+++ config/zellij/install.fish
@@ -0,0 +1,5 @@
+#!/usr/bin/env fish
+
+if command -qs cargo
+    cargo install --locked zellij
+end
